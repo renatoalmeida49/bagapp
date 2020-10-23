d9d6c1732956bc76802b4c872d90172a238a125c
Renato Novaes <renatoalmeida49@gmail.com>
2020-10-23 20:51:52 -0300

    Tag meta added in the template. Rokkie mistake.

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="icon" type="image/png" />
         <link rel="stylesheet" href="<?=$base;?>/assets/css/bootstrap.min.css" />
         <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css" />
